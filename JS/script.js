77506cb55b0d53a24399ff90fa11d563c9c699a0
koko11333333 <phattharadanaitreewiset@gmail.com>
2024-09-05 15:43:40 +0700

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+console.log("hello javas"); //เเสดงผลคอนโซล
+console.log("hello Thursday");// แสดงผลหน้าเว็ป
+document.getElementById("text").innerHTML="koko";//เปิดหน้าเว็ป
+document.write("hello document");  //ข้อความ
+
+window.alert("welcom to my page");  ///หน้าหมุนๆ
